7f9abde9301ee4e387b65698bfba89eece972779
jiscop85 <127202191+jiscop85@users.noreply.github.com>
2026-04-24 00:43:56 +0330

    Delete src/Index.tsx

--- src/Index.tsx
+++
@@ -1,5 +0,0 @@
-import { motion } from 'framer-motion';
-import { Film, Mic2, Calendar, Sparkles, Zap, Shield } from 'lucide-react';
-import { Link } from 'react-router-dom';
-import FeatureCard from '@/components/FeatureCard';
-
